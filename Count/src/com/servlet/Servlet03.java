9920435d21af8c153dacb130f9ead2d29bf981ee
CelestialVisionary <zilvdebao@outlook.com>
2025-09-20 15:18:14 +0800

    Update Tomcat from 8.5 to 11 and migrate to Jakarta EE

--- Count/src/com/servlet/Servlet03.java
+++ Count/src/com/servlet/Servlet03.java
@@ -1,10 +1,10 @@
 package com.servlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 //只是从Servlet02获取最新的计数量，它本身不刷新计数
--- login/src/sunyang/LoginServlet.java
+++ login/src/sunyang/LoginServlet.java
@@ -2,16 +2,16 @@ package sunyang;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 //@WebServlet("/login")
-public class LoginServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException, IOException {
+public class LoginServlet extends jakarta.servlet.http.HttpServlet {
+    protected void doPost(jakarta.servlet.http.HttpServletRequest req, jakarta.servlet.http.HttpServletResponse resp) throws jakarta.servlet.ServletException, IOException {
         resp.setCharacterEncoding("GBK");              //设置响应的编码类型为GBK
         PrintWriter out=resp.getWriter();                //获取输出对象
         out.println("<html>");
@@ -58,7 +58,7 @@ public class LoginServlet extends javax.servlet.http.HttpServlet {
 
 
 
-    protected void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException, IOException {
+    protected void doGet(jakarta.servlet.http.HttpServletRequest req, jakarta.servlet.http.HttpServletResponse resp) throws jakarta.servlet.ServletException, IOException {
         this.doPost(req, resp);
     }
 }
